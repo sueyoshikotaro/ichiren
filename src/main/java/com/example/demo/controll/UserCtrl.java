6302adbb1a1d74ea06577487056d98e5c553b818
minatohara <ichiren@kcs>
2024-12-19 15:58:34 +0900

    末吉確認用

--- src/main/java/com/example/demo/controll/UserCtrl.java
+++ src/main/java/com/example/demo/controll/UserCtrl.java
@@ -19,6 +19,8 @@ import com.example.demo.form.TaskForm;
 import com.example.demo.repository.UserCrudRepository;
 import com.example.demo.service.TaskServiceInterface;
 
+
+
 @Controller
 @RequestMapping("/taskdon/user")
 public class UserCtrl {
@@ -158,13 +160,35 @@ public class UserCtrl {
 	
 	/**
 	 * タスク登録確認画面を表示するリクエストハンドラメソッド
+	 * 湊原
+	 * @return
 	 */
-	public ModelAndView taskRegisterConfirm(TaskForm t, ModelAndView mav) {
+	@PostMapping("taskRegistConfirm")
+	public ModelAndView taskRegistConfirm(TaskForm t, ModelAndView mav) {
 		
 		mav.addObject("tasks", t);
 		mav.setViewName("leader/taskRegistConfirm");
 		return mav;
 	}
+	
+	/**
+	 * タスク登録完了画面を表示するリクエストハンドラメソッド
+	 * 湊原
+	 * @return
+	 */
+	@PostMapping("taskRegistComplete")
+	public ModelAndView taskRegistComplete(ModelAndView mav, TaskForm t) {
+		System.out.println(t);
+//		if () {
+//			
+//		}
+		TaskService.taskRegister(5, t.getTask_category(),t.getTask_name(),t.getTask_content(),"未着手"
+				, t.getStart_date(), t.getEnd_date(), t.getTask_priority(),t.getTask_level(),t.getTask_weight()
+				, 0,1,t.getUser_id(),t.getGroup_id());
+		mav.setViewName("leader/taskRegistComplete");
+		return mav;
+	}
+	
 	
 	
 }
