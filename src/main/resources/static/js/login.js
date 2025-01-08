3947fb9a613430d973625800f99b2cc92c426ca1
kusumoto <iciren@kcs>
2025-01-08 16:03:08 +0900

    変更しろ殺すぞ

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -7,3 +7,19 @@ document.querySelector('.popup-link').addEventListener('click', function() {
 document.querySelector('.close').addEventListener('click', function() {
 	document.querySelector('.popup').style.display = 'none';
 });
+
+//目マーク動作(パスワード入力欄)
+const passwordToggle = document.querySelector('.input-group-text');
+const passwordInput = document.querySelector('#user_pass');
+
+passwordToggle.addEventListener('click', () => {
+	if (passwordInput.type === 'password') {
+		passwordInput.type = 'text';
+		document.querySelector('#eye').style.display = 'none';
+		document.querySelector('#eye-slash').style.display = 'block';
+	} else {
+		passwordInput.type = 'password';
+		document.querySelector('#eye').style.display = 'block';
+		document.querySelector('#eye-slash').style.display = 'none';
+	}
+});
