16cc27499fe070638aa130d9ead1f6837baaaa29
kusumoto <iciren@kcs>
2025-01-10 16:11:12 +0900

    変更及び修正と追加_楠元

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,3 +1,4 @@
+//下記ログイン画面(動作)
 // パスワード再設定説明のポップアップを表示する
 document.querySelector('.popup-link').addEventListener('click', function() {
 	document.querySelector('.popup').style.display = 'block';
@@ -9,6 +10,7 @@ document.querySelector('.close').addEventListener('click', function() {
 });
 
 //目マーク動作(パスワード入力欄)
+//パスワード入力後に、目マークが消えて再び再入力する際に、見え隠しできない為の処理
 const passwordToggle = document.querySelector('.input-group-text');
 const passwordInput = document.querySelector('#user_pass');
 
