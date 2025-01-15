97974305c69543e008f43bab617e235fcbbc9596
sueyoshi <ichiren@kcs>
2025-01-15 10:24:01 +0900

    グループ作成確認

--- src/main/java/com/example/demo/controll/AdminCtrl.java
+++ src/main/java/com/example/demo/controll/AdminCtrl.java
@@ -865,6 +865,7 @@ public class AdminCtrl {
 		//リーダ以外のメンバ
 		List<String> uncheckedUserId = new ArrayList<>();
 
+		System.out.println("kita!!!");
 		
 		if (userId != null && check != null) {
 			for (int i = 0; i < userId.length; i++) {
@@ -874,9 +875,15 @@ public class AdminCtrl {
 					uncheckedUserId.add(userId[i]);
 				}
 			}
+			//ユーザが選択されていない場合
 		} else if(userId == null) {
+			
 			mav.addObject("Msg", "ユーザを選択してください");
-			mav.setViewName(null);
+			mav.setViewName("admin/groupCreate");
+			
+		} else {
+			mav.addObject("Msg", "一人以上のリーダを選択してください");
+			mav.setViewName("admin/groupCreate");
 		}
 		
 		System.out.println("checkyu-zaId");
